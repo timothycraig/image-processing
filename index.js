13d6c4ddf4117807a13d1309e03da5b4d22aaa51
Timothy Craig <tcraig@frameshift.io>
2023-09-06 15:02:28 -0400

    fixes resizing issue

--- index.js
+++ index.js
@@ -1,51 +1,173 @@
 const fs = require('fs')
 const path = require('path')
 const sharp = require('sharp')
+const chalk = require('chalk')
 
-const imagesDir = path.resolve(__dirname, 'unprocessed')
-const outputDir = path.resolve(__dirname, 'processed')
+function getAllImageFilesInDirectory(dirPath, fileArray, relativePath = '') {
+  const files = fs.readdirSync(dirPath)
 
-if (!fs.existsSync(outputDir)){
-  fs.mkdirSync(outputDir);
-} else {
-  fs.rmdirSync(outputDir, { recursive: true })
+  files.forEach((file) => {
+    const filePath = path.join(dirPath, file)
+    const relativeFilePath = path.join(relativePath, file)
+    const stat = fs.statSync(filePath)
 
-  fs.mkdirSync(outputDir);
+    if (stat.isFile() && isImageFile(filePath)) {
+      fileArray.push({ filePath, relativeFilePath })
+    } else if (stat.isDirectory()) {
+      getAllImageFilesInDirectory(filePath, fileArray, relativeFilePath)
+    }
+  })
 }
 
-async function processImage (file) {
-  const filePath = path.resolve(imagesDir, file)
-  const thumbnailFileName = `${path.basename(file, path.extname(file))}-thumb${path.extname(file)}`
+function isImageFile(filePath) {
+  const imageExtensions = ['.jpg', '.jpeg', '.png', '.gif']
+  const ext = path.extname(filePath).toLowerCase()
+  return imageExtensions.includes(ext)
+}
+
+function displayFileSize(bytes, noColor = false) {
+  const kb = (bytes / 1024).toFixed(1)
+
+  if (noColor) return chalk.white(`${kb} KB`)
+
+  if (kb > 500) {
+    return chalk.red(`${kb} KB`)
+  } else if (kb > 250) {
+    return chalk.yellow(`${kb} KB`)
+  } else {
+    return chalk.green(`${kb} KB`)
+  }
+}
+
+function calcPercentChange(oldSize, newSize) {
+  return Number((((oldSize - newSize) / oldSize) * 100).toFixed(1))
+}
+
+async function compressImage(fileInfo, outputDir) {
+  const { filePath, relativeFilePath } = fileInfo
+  const outputPath = path.join(outputDir, relativeFilePath)
+  const ext = path.extname(filePath).toLowerCase()
+  let wasResized = false
+  let wasCompressed = true
+  let origStats
+  let processedStats
+  let processedMeta
+
+  // Ensure the output directory exists before writing the file
+  await fs.promises.mkdir(path.dirname(outputPath), { recursive: true })
+
+  const metadata = await sharp(filePath).metadata()
 
   try {
-    const fullSize = await sharp(filePath)
-      .resize({
-        width: 600,
-        height: null,
-      })
-      .jpeg({ quality: 90 })
-      .withMetadata()
-      .toFile(path.resolve(outputDir, file))
+    switch (ext) {
+      case '.png':
+        if (metadata.width > 1920) {
+          await sharp(filePath)
+            .resize({
+              width: 1920,
+              height: null,
+            })
+            .png({ quality: 80 })
+            .toFile(outputPath)
 
-    const thumbnailSize = await sharp(filePath)
-      .resize({
-        width: null,
-        height: 50,
-      })
-      .jpeg({ quality: 90 })
-      .withMetadata()
-      .toFile(path.resolve(outputDir, thumbnailFileName))
+          wasResized = true
+        } else {
+          await sharp(filePath)
+            .png({ quality: 80 })
+            .toFile(outputPath)
+        }
 
-    console.log(fullSize, thumbnailSize)
+        origStats = await fs.promises.stat(filePath)
+        processedStats = await fs.promises.stat(outputPath)
+        processedMeta = await sharp(outputPath).metadata()
+
+        if (calcPercentChange(origStats.size, processedStats.size) > 30 || wasResized) {
+          console.log(`${displayFileSize(origStats.size)} -> ${displayFileSize(processedStats.size)} ${chalk.dim(`${calcPercentChange(origStats.size, processedStats.size)}%`)} ${chalk.underline(path.basename(outputPath))} ${chalk.italic(`(${processedMeta.width} X ${processedMeta.height})`)}`)
+        } else {
+          await fs.promises.cp(filePath, outputPath)
+
+          console.log(`${displayFileSize(origStats.size)} -> ${displayFileSize(origStats.size, true)} ${chalk.dim('N/A')} ${chalk.underline(path.basename(outputPath))} ${chalk.italic(`(${processedMeta.width} X ${processedMeta.height})`)}`)
+
+          wasCompressed = false
+        }
+
+        return [wasCompressed ? 1 : 0, wasResized ? 1 : 0]
+      case '.jpeg':
+      case '.jpg':
+        if (metadata.width > 1920) {
+          await sharp(filePath)
+            .resize({
+              width: 1920,
+              height: null,
+            })
+            .jpeg({ quality: 80 })
+            .toFile(outputPath)
+
+          wasResized = true
+        } else {
+          await sharp(filePath)
+            .jpeg({ quality: 80 })
+            .toFile(outputPath)
+        }
+
+        origStats = await fs.promises.stat(filePath)
+        processedStats = await fs.promises.stat(outputPath)
+        processedMeta = await sharp(outputPath).metadata()
+
+        if (calcPercentChange(origStats.size, processedStats.size) > 30 || wasResized) {
+          console.log(`${displayFileSize(origStats.size)} -> ${displayFileSize(processedStats.size)} ${chalk.dim(`${calcPercentChange(origStats.size, processedStats.size)}%`)} ${chalk.underline(path.basename(outputPath))} ${chalk.italic(`(${processedMeta.width} X ${processedMeta.height})`)}`)
+        } else {
+          await fs.promises.cp(filePath, outputPath)
+
+          console.log(`${displayFileSize(origStats.size)} -> ${displayFileSize(origStats.size, true)} ${chalk.dim('N/A')} ${chalk.underline(path.basename(outputPath))} ${chalk.italic(`(${processedMeta.width} X ${processedMeta.height})`)}`)
+
+          wasCompressed = false
+        }
+
+        return [wasCompressed ? 1 : 0, wasResized ? 1 : 0]
+      default:
+        await fs.promises.cp(filePath, outputPath)
+
+        origStats = await fs.promises.stat(filePath)
+        processedMeta = await sharp(outputPath).metadata()
+
+        console.log(`${displayFileSize(origStats.size, true)} -> ${displayFileSize(origStats.size, true)} ${chalk.dim('N/A')} ${chalk.underline(path.basename(outputPath))} ${chalk.italic(`(${processedMeta.width} X ${processedMeta.height})`)}`)
+
+        return [0, 0]
+    }
   } catch (error) {
-    console.error(error)
+    console.error(`Error compressing ${filePath}: ${error.message}`)
   }
+}
+
+const sourceDirectory = path.resolve(__dirname, 'unprocessed')
+const outputDirectory = path.resolve(__dirname, 'processed')
+
+if (!fs.existsSync(outputDirectory)) {
+  fs.mkdirSync(outputDirectory, { recursive: true })
+} else {
+  fs.rmSync(outputDirectory, { recursive: true })
+
+  fs.mkdirSync(outputDirectory, { recursive: true })
 }
 
 (async () => {
-  const files = fs.readdirSync(imagesDir)
+  let totalNumCompressed = 0
+  let totalNumResized = 0
+  const imageFiles = []
 
-  for (const file of files) {
-    await processImage(file)
+  getAllImageFilesInDirectory(sourceDirectory, imageFiles)
+
+  for (const fileInfo of imageFiles) {
+    const [numCompressed, numResized] = await compressImage(fileInfo, outputDirectory)
+
+    totalNumCompressed = totalNumCompressed + numCompressed
+    totalNumResized = totalNumResized + numResized
   }
+
+  console.log('----------------------------')
+  console.log(`${imageFiles.length} Image Files Found`)
+  console.log(`${totalNumCompressed} Files Compressed`)
+  console.log(`${totalNumResized} Files Resized`)
+  console.log('----------------------------')
 })()
